decf7873d037b982c85c4a51073cd3d06552eedc
SagniktaNathChaudhuri <149265180+SagniktaNathChaudhuri@users.noreply.github.com>
2025-01-30 08:44:37 +0530

    Add files via upload

---
+++ Railway_Ticket_Reservation-main/Railway_Ticket_Reservation-main/RailwayTicketReservation.c
@@ -0,0 +1,443 @@
+/***************PREPROCESSORS*******************/
+//Train Reservation System - BROUGHT TO YOU BY :dwij
+//Working on basic structure
+//including all libraries for now
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/**************GLOBAL VARIABLES*******************/
+//ALl the globle variables and the composite data types will be declared here
+typedef struct{
+	char name[50];
+	int train_num;
+	int num_of_seats;
+}pd;
+
+
+
+
+/**************FUNCTION PROTOTYPE*******************/
+//function prototypes to be used
+void reservation(void);							//main reservation function
+void viewdetails(void);							//view details of all the trains
+void cancel(void);								
+void printticket(char name[],int,int,float);	//print ticket 
+void specifictrain(int);						//print data related to specific train
+float charge(int,int);							//charge automatically w.r.t number of seats and train
+void login();
+int showPassengers();
+
+/***************FUNCTION DECLARATION*******************/
+
+/****************MAIN()****************/
+
+int main()
+
+{
+		system("cls"); 
+	printf("\t\t=================================================\n");
+	printf("\t\t|                                               |\n");
+	printf("\t\t|        -----------------------------          |\n");
+	printf("\t\t|           TRAIN TICKET RERS. SYSTEM           |\n");
+	printf("\t\t|        -----------------------------          |\n");
+	printf("\t\t|                                               |\n");
+	printf("\t\t=================================================\n\n\n");
+		
+	    
+	printf(" \n Press any key to continue:");
+	
+	getch();	
+    system("cls");
+	login();
+	int menu_choice,choice_return;
+	start:
+	system("cls");
+	printf("\n=================================\n");
+	printf("    TRAIN RESERVATION SYSTEM");
+	printf("\n=================================");
+	printf("\n1>> Reserve A Ticket");
+	printf("\n------------------------");
+	printf("\n2>> View All Available Trains");
+	printf("\n------------------------");
+	printf("\n3>> Cancel Reservation");
+	printf("\n------------------------");
+	printf("\n4>> Show Passengers");
+	printf("\n------------------------");
+	printf("\n5>> Exit");
+	printf("\n------------------------");
+	printf("\n\n-->");
+	scanf("%d",&menu_choice);
+	switch(menu_choice)
+	{
+		case 1:
+			reservation();		//Fucntion still not added
+			break;
+		case 2:
+			viewdetails();
+			printf("\n\nPress any key to go to Main Menu..");
+			getch();
+			break;
+		case 3:
+			cancel();
+			break;
+		case 4:
+			showPassengers();
+			break;
+		case 5:
+			return(0);
+		default:
+			printf("\nInvalid choice");
+	}
+	goto start;
+	return(0);
+}
+
+/****************VIEWDETAILS()****************/
+
+//The function is yet not completed, need more details to be added!
+//timings of the trains are still missing 
+
+void viewdetails(void)
+{
+	system("cls");
+	printf("-----------------------------------------------------------------------------");	
+	printf("\nTr.No\tName\t\t\tDestinations\t\tCharges\t\tTime\n");
+	printf("-----------------------------------------------------------------------------");
+	printf("\n1001\tRed Lines Express\tBoston to Manhattan\tRs.5000\t\t9am");
+	printf("\n1002\tRed Lines Express\tManhattan To Boston\tRs.5000\t\t12pm");
+	printf("\n1003\tLA City Express\t\tBoston To L.A\t\tRs.4500\t\t8am");
+	printf("\n1004\tLA City Express\t\tL.A To Boston\t\tRs.4500\t\t11am");
+	printf("\n1005\tIron City Express\tBoston To Chicago\tRs.4000\t\t7am");
+	printf("\n1006\tIron City Express\tChicago To Boston\tRs.4000\t\t9.30am");
+    printf("\n1007\tKeystone Express\tBoston To Washington\tRs.3500\t\t1pm");	
+    printf("\n1008\tKeystone Express\tWashington To Boston\tRs.3500\t\t4pm");
+    printf("\n1009\tMeteor Express\t\tBoston To Miami\t\tRs.6000\t\t3.35pm");
+    printf("\n1009\tMeteor Express\t\tMiami To Boston\t\tRs.6000\t\t4.15pm");
+    
+}
+
+/****************RESERVATION()****************/
+
+void reservation(void)
+{
+	char confirm;
+	int i=0;
+	float charges;
+	pd passdetails;
+	FILE *fp;
+	fp=fopen("seats_reserved.txt","a");
+	system("cls");
+	
+	printf("\nEnter Your Name:> ");
+	fflush(stdin);
+	gets(passdetails.name);
+	//error here have to take input of the name 
+	printf("\nEnter Number of seats:> ");
+	scanf("%d",&passdetails.num_of_seats);
+	printf("\n\n>>Press Enter To View Available Trains<< ");
+	getch();
+	system("cls");
+	viewdetails();
+	printf("\n\nEnter train number:> ");
+	start1:
+	scanf("%d",&passdetails.train_num);
+	if(passdetails.train_num>=1001 && passdetails.train_num<=1010)
+	{
+		charges=charge(passdetails.train_num,passdetails.num_of_seats);
+		printticket(passdetails.name,passdetails.num_of_seats,passdetails.train_num,charges);		
+	}
+	else
+	{
+		printf("\nInvalid train Number! Enter again--> ");
+		goto start1;
+	}
+	
+	printf("\n\nConfirm Ticket (y/n):>");
+	start:
+	scanf(" %c",&confirm);
+	if(confirm == 'y')
+	{
+		fprintf(fp,"%s\t\t%d\t\t%d\t\t%.2f\n",&passdetails.name,passdetails.num_of_seats,passdetails.train_num,charges);
+		printf("==================");
+		printf("\n Reservation Done\n");
+		printf("==================");
+		printf("\nPress any key to go back to Main menu");
+	}
+	else
+	{
+		if(confirm=='n'){
+			printf("\nReservation Not Done!\nPress any key to go back to  Main menu!");
+		}
+		else
+		{
+			printf("\nInvalid choice entered! Enter again-----> ");
+			goto start;
+		}
+	}
+	fclose(fp);
+	getch();
+}
+
+/****************CHARGE()****************/
+
+float charge(int train_num,int num_of_seats)
+{
+		if (train_num==1001)
+	{
+		return(5000.0*num_of_seats);
+	}
+	if (train_num==1002)
+	{
+		return(5000.0*num_of_seats);
+	}
+	if (train_num==1003)
+	{
+		return(4500.0*num_of_seats);
+	}
+	if (train_num==1004)
+	{
+		return(4500.0*num_of_seats);
+	}
+	if (train_num==1005)
+	{
+		return(4000.0*num_of_seats);
+	}
+	if (train_num==1006)
+	{
+		return(4000.0*num_of_seats);
+	}
+	if (train_num==1007)
+	{
+		return(3500.0*num_of_seats);
+	}
+	if (train_num==1008)
+	{
+		return(3500.0*num_of_seats);
+	}
+	if (train_num==1009)
+	{
+		return(6000.0*num_of_seats);
+	}
+	if (train_num==1010)
+	{
+		return(6000.0*num_of_seats);
+	}
+}
+
+
+/****************PRINTTICKET()****************/
+
+void printticket(char name[],int num_of_seats,int train_num,float charges)
+{
+	system("cls");
+	printf("-------------------\n");
+	printf("\tTICKET\n");
+	printf("-------------------\n\n");
+	printf("Name:\t\t\t%s",name);
+	printf("\nNumber Of Seats:\t%d",num_of_seats);
+	printf("\nTrain Number:\t\t%d",train_num);
+	specifictrain(train_num);
+	printf("\nCharges:\t\t%.2f",charges);
+}
+
+/****************SPECIFICTRAIN()****************/
+
+void specifictrain(int train_num)
+{
+	
+	if (train_num==1001)
+	{
+		printf("\nTrain:\t\t\tRed Lines Express");
+		printf("\nDestination:\t\tBoston to Manhattan");
+		printf("\nDeparture:\t\t9am ");
+	}
+	if (train_num==1002)
+	{
+		printf("\nTrain:\t\t\tRed Lines Express");
+		printf("\nDestination:\t\tManhattan to Boston");
+		printf("\nDeparture:\t\t12pm");
+	}
+	if (train_num==1003)
+	{
+		printf("\nTrain:\t\t\tLA City Express");
+		printf("\nDestination:\t\tBoston to L.A");
+		printf("\nDeparture:\t\t8am");
+	}
+	if (train_num==1004)
+	{
+		printf("\nTrain:\t\t\tLA City Express");
+		printf("\nDestination:\t\tL.A to Boston");
+		printf("\nDeparture:\t\t11am ");
+	}
+	if (train_num==1005)
+	{
+		printf("\nTrain:\t\t\tIron City Express");
+		printf("\nDestination:\t\tBoston to Chicago");
+		printf("\nDeparture:\t\t7am");
+	}
+	if (train_num==1006)
+	{
+		printf("\ntrain:\t\t\tIron City Express");
+		printf("\nDestination:\t\tChicago to Boston");
+		printf("\nDeparture:\t\t9.30am ");
+	}
+	if (train_num==1007)
+	{
+		printf("\ntrain:\t\t\tKeystone Express");
+		printf("\nDestination:\t\tBoston to Washington");
+		printf("\nDeparture:\t\t1pm ");
+	}
+	if (train_num==1008)
+	{
+		printf("\ntrain:\t\t\tKeystone Express");
+		printf("\n Destination:\t\tWashington to Boston");
+		printf("\nDeparture:\t\t4pm ");
+	}
+	if (train_num==1009)
+	{
+		printf("\ntrain:\t\t\tMeteor Express");
+		printf("\nDestination:\t\tBoston to Miami");
+		printf("\nDeparture:\t\t3.35pm ");
+	}
+	if (train_num==1010)
+	{
+		printf("\ntrain:\t\t\tMeteor Express");
+		printf("\nDestination:\t\tMiami to Boston");
+		printf("\nDeparture:\t\t1.15 ");
+	}
+}
+
+void login()
+{
+	int a=0,i=0;
+    char uname[10],c=' '; 
+    char pword[10],code[10];
+    char user[10];
+    char pass[10];
+    do
+{
+	
+    printf("\n  =======================  LOGIN FORM  =======================\n  ");
+    printf(" \n                       ENTER USERNAME:-");
+	scanf("%s", &uname); 
+	printf(" \n                       ENTER PASSWORD:-");
+	while(i<10)
+	{
+	    pword[i]=getch();
+	    c=pword[i];
+	    if(c==13) break;
+	    else printf("*");
+	    i++;
+	}
+	pword[i]='\0';
+	//char code=pword;
+	i=0;
+	//scanf("%s",&pword); 
+		if(strcmp(uname,"admin")==0 && strcmp(pword,"admin")==0)
+	{
+	printf("  \n\n\n       WELCOME TO OUR SYSTEM !! YOUR LOGIN IS SUCCESSFUL");
+	printf("\n\n\n\t\t\t\tPress any key to continue...");
+	getch();//holds the screen
+	break;
+	}
+	else
+	{
+		printf("\n        SORRY !!!!  LOGIN IS UNSUCESSFUL");
+		a++;
+		
+		getch();//holds the screen
+		system("cls");
+	}
+}
+	while(a<=2);
+	if (a>2)
+	{
+		printf("\nSorry you have entered the wrong username and password for four times!!!");
+		
+		getch();
+		
+		}
+		system("cls");	
+}
+
+int showPassengers() 
+{ 
+	FILE *fptr;
+    char c; 
+    // Open file 
+    fptr = fopen("seats_reserved.txt", "r"); 
+    if (fptr == NULL) 
+    { 
+        printf("Cannot open file \n"); 
+        exit(0); 
+    } 
+    system("cls");
+	printf("-----------------------------------------------------------------------------\n");
+    printf("%s\t\t%s\t%s\t%s\n","Name:","No of Seats:","Train No:","Charges:");
+	printf("-----------------------------------------------------------------------------\n");
+    // Read contents from file 
+    c = fgetc(fptr); 
+    while (c != EOF) 
+    { 
+        printf ("%c", c); 
+        c = fgetc(fptr); 
+    } 
+	printf("\n\n\n\t\t\t\tPress any key to continue...");
+	getch();
+    fclose(fptr); 
+    return 0; 
+}
+
+void cancel(void)   
+{
+	system("cls");
+	FILE *fp1, *fp2;
+    //consider 40 character string to store filename
+    char c;
+    int del_line, temp = 1;
+    //open file in read mode
+    printf("-----------------------------------------------------------------------------\n");
+    printf("%s\t\t%s\t%s\t%s\n","Name:","No of Seats:","Train No:","Charges:");
+	printf("-----------------------------------------------------------------------------\n");
+    fp1 = fopen("seats_reserved.txt", "r");
+    c = getc(fp1);
+    //until the last character of file is obtained
+    while (c != EOF)
+    {
+      printf("%c", c);
+      //print current character and read next character
+      c = getc(fp1);
+    }
+    //rewind
+    rewind(fp1);
+    printf(" \n\n\t\t Enter line number for passenger to be deleted:");
+    //accept number from user.
+    scanf("%d", &del_line);
+    //open new file in write mode
+    fp2 = fopen("copy.txt", "w+");
+    c = getc(fp1);
+    while (c != EOF) {
+      c = getc(fp1);
+      if (c==EOF)
+       break;
+      if (c == '\n')
+      	temp++;
+      //except the line to be deleted
+      if (temp != del_line)
+      {
+        //copy all lines in file copy.txt
+        putc(c, fp2);
+      }
+    }
+    //close both the files.
+    fclose(fp1);
+    fclose(fp2);
+    //remove original file
+    remove("seats_reserved.txt");
+    //rename the file copy.c to original name
+    rename("copy.txt","seats_reserved.txt");
+    printf("\n The contents of file after being modified are as follows:\n");
+    showPassengers();
+}
